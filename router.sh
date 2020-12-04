6968f2a178172487f56132284d48fddfee410e33
Henrik Lagrosen <henrikl@tuta.io>
2020-12-04 20:21:31 +0100

    port change

--- router.sh
+++ router.sh
@@ -4,6 +4,6 @@ if [ $1 = "resume" ]; then #called when awaken from suspend
     echo $1
     
     #replace with your own scripts
-    su h -c "DISPLAY=:0 sensible-browser localhost:8080/pause-checklist"
+    su h -c "DISPLAY=:0 sensible-browser localhost:9991/pause-checklist"
 fi
 
